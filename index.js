e40cf349c2956052b453976b90f7180b7aa2dacc
David Parsons <info@dpdesigns.eu>
2021-05-10 22:00:13 +0200

    updated socket.io - updated production url

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.set("trust proxy", true);
 const server = require("http").Server(app);
 // const io = require("socket.io")(server, { origins: "localhost:8080" funkychicken.heroku.com:*});
 const io = require("socket.io")(server, {
-    origins: "localhost:8080 zero-socialnetwork.herokuapp.com"
+    origins: "localhost:8080 zero-socialnetwork.herokuapp.com:*"
 });
 
 // const io = require("socket.io")(server, { origins: "localhost:8080" });
