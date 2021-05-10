163269843ebafb43bf7d20ffdebd7cc15ce272ac
David Parsons <info@dpdesigns.eu>
2021-05-10 19:17:05 +0200

    updated socket.io configuration

--- src/socket.js
+++ src/socket.js
@@ -4,9 +4,20 @@ import { allChatMessages, chatMessage } from "./actions/actions";
 
 export let socket;
 
+// export const init = store => {
+//     if (!socket) {
+//         socket = io.connect();
+//         socket.on("allChatMessages", msgs =>
+//             store.dispatch(allChatMessages(msgs))
+//         );
+//         socket.on("chatMessage", msg => store.dispatch(chatMessage(msg)));
+//     }
+// };
 export const init = store => {
     if (!socket) {
-        socket = io.connect();
+        socket = io.connect("https://zero-socialnetwork.herokuapp.com/", {
+            secure: true
+        });
         socket.on("allChatMessages", msgs =>
             store.dispatch(allChatMessages(msgs))
         );
